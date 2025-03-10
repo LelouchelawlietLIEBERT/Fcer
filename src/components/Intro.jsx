6ac009e2b0c318df61622abcb08a77263ec15b2e
LelouchelawlietLIEBERT <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-03-10 21:14:16 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -334,60 +334,202 @@ const Intro = () => {
           <div className="trigger"></div>
           <div className="section">
             <h1 style={{ fontSize: "45px" }}>Hi I'm ASHWIN</h1>
-            <h3>This is My Journey</h3>
-            <p>Get ready for the ride!!!</p>
-            <div className="scroll-cta">Scroll</div>
+            <h3>This is My Boring Description</h3>
+            <p>Enjoy the ride getting bored while reading if you want to</p>
+            <div className="scroll-cta">Scroll down gently</div>
           </div>
 
           <div className="section right">
-            <h2>They're kinda like buses...</h2>
+            <h2>Basically I'm into ... </h2>
           </div>
 
           <div className="ground-container">
             <div className="parallax ground"></div>
             <div className="section right">
-              <h2>..except they leave the ground.</h2>
-              <p>Saaay what!?.</p>
+              <h2>1. Web Development</h2>
+              <p style={{ fontSize: "25px" }}>
+                coming up with creative designs.. n .. all
+              </p>
             </div>
 
             <div className="section">
-              <h2>They fly through the sky.</h2>
-              <p>For realsies!</p>
+              <h2>2. Data Science</h2>
+              <p style={{ fontSize: "25px" }}>
+                Love python, so have grasp of its libraries.. n .. all{" "}
+              </p>
             </div>
 
             <div className="section right">
-              <h2>Defying all known physical laws.</h2>
-              <p>It's actual magic!</p>
+              <h2>3. Mathematics</h2>
+              <p style={{ fontSize: "25px" }}>
+                I love Math most of all, especially calculus and statistics, its
+                just awesome ...
+              </p>
             </div>
             <div className="parallax clouds"></div>
           </div>
 
           <div className="blueprint">
             <div className="section dark">
-              <h2>The facts and figures.</h2>
-              <p>Lets get into the nitty gritty...</p>
+              <h2>About Me : </h2>
+              <p
+                style={{
+                  width: "600px",
+                  lineHeight: "40px",
+                  color: "rgb(21, 255, 0)",
+                }}
+              >
+                Meet Ashwin Gowda (Myself), a React enthusiast with a 9.2 GPA
+                and a deep love for math. Currently studying at Ramaiah
+                Institute of Technology in Bangalore, they're all about building
+                sleek UIs and solving data science problems, all while cracking
+                numbers like it's a game. From Python to Go, I love every
+                programming language they meet, and they’re always ready to
+                solve bugs and equations with equal enthusiasm.
+              </p>
             </div>
-            <div className="section dark length">
-              <h2>Length.</h2>
-              <p>Long.</p>
+            <div
+              className="section dark length"
+              style={{ marginBottom: "100px" }}
+            >
+              <h2>Tech and Skills</h2>
+              <h3 style={{ marginTop: "80px", color: "rgb(203, 255, 32)" }}>
+                Web Development :{" "}
+              </h3>
+              <ul>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="html.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>HTML</h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="css.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>CSS</h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="js.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    JAVASCRIPT
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="react.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    REACT JS
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="nodejs.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    NODE JS
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "40px" }}>
+                    <img style={{ width: "100%" }} src="mongodb.svg" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    MONGO DB
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "40px" }}>
+                    <img style={{ width: "100%" }} src="flask.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    FLASK (PYTHON)
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="express.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    EXPRESS JS
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="mysql.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    MYSQL DB
+                  </h4>
+                </li>
+              </ul>
             </div>
             <div className="section dark wingspan">
-              <h2>Wing Span.</h2>
-              <p>I dunno, longer than a cat probably.</p>
-            </div>
-            <div className="section dark phalange">
-              <h2>Left Phalange</h2>
-              <p>Missing</p>
+              <h3 style={{ marginTop: "100px", color: "rgb(203, 255, 32)" }}>
+                Data Science :{" "}
+              </h3>
+              <p>Currently Learning ...</p>
             </div>
             <div className="section dark">
-              <h2>Engines</h2>
-              <p>Turbine funtime</p>
+              <h3 style={{ color: "rgb(203, 255, 32)" }}>
+                Programming Languages :{" "}
+              </h3>
+              <ul>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="python.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    PYTHON
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="java.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>JAVA</h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="c.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>C</h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="cpp.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>C++</h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "30px" }}>
+                    <img style={{ width: "100%" }} src="js.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>
+                    JAVASCRIPT
+                  </h4>
+                </li>
+                <li style={{ display: "flex", cursor: "pointer" }}>
+                  <div style={{ width: "50px" }}>
+                    <img style={{ width: "100%" }} src="go.png" alt="" />
+                  </div>
+                  <h4 style={{ marginTop: "5px", marginLeft: "10px" }}>GO</h4>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="sunset">
             <div className="section"></div>
             <div className="section end">
-              <h2>Fin.</h2>
+              <h2>Thanks for getting Bored!!</h2>
+              <p>
+                {" "}
+                if you are wondering about email and contacting go to main page
+              </p>
+              <a href="/">Click here to go back</a>
             </div>
           </div>
         </div>
