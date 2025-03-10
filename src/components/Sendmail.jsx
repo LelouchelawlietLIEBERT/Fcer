9ee9f250086feea53093c6749ecc1271ab882b8c
LelouchelawlietLIEBERT <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-03-10 19:11:10 +0530

    Update Sendmail.jsx

--- src/components/Sendmail.jsx
+++ src/components/Sendmail.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import Mailform from "./Mailform";
-import Gbl3 from "./Gbl3";
+import React, { useState } from "react";
 
 const Sendmail = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
   return (
     <>
       <div className="grid-container">
@@ -17,8 +19,66 @@ const Sendmail = () => {
       </div>
       <div className="sendmail">
         <h1 className="h1regi">SEND MAIL</h1>
-        <Gbl3 />
-        <Mailform />
+        <div style={{ marginTop: "50px", width: "560px" }}>
+          <form className="my-form">
+            <div className="container">
+              <ul>
+                <li>
+                  <div className="grid grid-2">
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" required />
+                  </div>
+                </li>
+                <li>
+                  <textarea placeholder="Message"></textarea>
+                </li>
+                <li>
+                  <input
+                    type="checkbox"
+                    id="terms"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                  />
+                  <label htmlFor="terms">
+                    I have read and agreed with {"  "}
+                    <a href="">the terms and conditions.</a>
+                  </label>
+                </li>
+                <li>
+                  <div className="grid grid-3">
+                    <div className="required-msg">REQUIRED FIELDS</div>
+                    <button
+                      className="btn-grid"
+                      type="submit"
+                      disabled={!isChecked}
+                    >
+                      <span className="back">
+                        <img
+                          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/email-icon.svg"
+                          alt=""
+                        />
+                      </span>
+                      <span className="front">SUBMIT</span>
+                    </button>
+                    <button
+                      className="btn-grid"
+                      type="reset"
+                      disabled={!isChecked}
+                    >
+                      <span className="back">
+                        <img
+                          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/eraser-icon.svg"
+                          alt=""
+                        />
+                      </span>
+                      <span className="front">RESET</span>
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
